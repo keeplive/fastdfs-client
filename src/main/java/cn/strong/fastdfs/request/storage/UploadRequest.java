2b73d05100492717f3233541b284baba967b1899
liulongbiao <liulongbiao@cnstrong.cn>
2016-02-26 18:03:03 +0800

    add AbstractFileRequest

--- src/main/java/cn/strong/fastdfs/request/storage/UploadRequest.java
+++ src/main/java/cn/strong/fastdfs/request/storage/UploadRequest.java
@@ -2,30 +2,16 @@
  * 
  */
 package cn.strong.fastdfs.request.storage;
-import static cn.strong.fastdfs.core.Consts.ERRNO_OK;
 import static cn.strong.fastdfs.core.Consts.FDFS_FILE_EXT_LEN;
 import static cn.strong.fastdfs.core.Consts.FDFS_PROTO_PKG_LEN_SIZE;
 import static cn.strong.fastdfs.core.Consts.FDFS_STORE_PATH_INDEX_LEN;
-import static cn.strong.fastdfs.core.Consts.HEAD_LEN;
 import static cn.strong.fastdfs.util.Helper.writeFixLength;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
-import io.netty.buffer.CompositeByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.Channel;
-import io.netty.channel.DefaultFileRegion;
-import io.netty.channel.FileRegion;
-import io.netty.handler.stream.ChunkedInput;
-import io.netty.handler.stream.ChunkedNioStream;
-import io.netty.handler.stream.ChunkedStream;
 
 import java.io.File;
-import java.io.InputStream;
-import java.nio.channels.ReadableByteChannel;
-import java.util.Objects;
 
 import cn.strong.fastdfs.core.CommandCodes;
-import cn.strong.fastdfs.request.Sender;
 import cn.strong.fastdfs.util.Helper;
 
 /**
@@ -34,77 +20,29 @@ import cn.strong.fastdfs.util.Helper;
  * @author liulongbiao
  *
  */
-public class UploadRequest implements Sender {
+public class UploadRequest extends AbstractFileRequest {
 
-	public final Object content;
-	public final long length;
 	public final String ext;
 	public final byte storePathIndex;
 
 	public UploadRequest(File file, byte storePathIndex) {
-		if (!file.exists()) {
-			throw new IllegalArgumentException("uploaded file " + file.getName() + " not exist");
-		}
-		long length = file.length();
-		this.content = new DefaultFileRegion(file, 0, length);
-		this.length = length;
+		super(file);
 		this.ext = Helper.getFileExt(file.getName());
 		this.storePathIndex = storePathIndex;
 	}
 
-	public UploadRequest(Object content, long length, String ext, byte storePathIndex) {
-		this.content = toContent(content);
-		this.length = length;
+	public UploadRequest(Object content, long size, String ext, byte storePathIndex) {
+		super(content, size);
 		this.ext = ext;
 		this.storePathIndex = storePathIndex;
 	}
 
-	private static Object toContent(Object content) {
-		Objects.requireNonNull(content, "uploaded content shoule not be null");
-		if (content instanceof ByteBuf || content instanceof FileRegion
-				|| content instanceof ChunkedInput) {
-			return content;
-		} else if (content instanceof File) {
-			File file = (File) content;
-			if (!file.exists()) {
-				throw new IllegalArgumentException("uploaded file " + file.getName() + " not exist");
-			}
-			return new DefaultFileRegion(file, 0, file.length());
-		} else if (content instanceof ReadableByteChannel) {
-			return new ChunkedNioStream((ReadableByteChannel) content);
-		} else if (content instanceof InputStream) {
-			return new ChunkedStream((InputStream) content);
-		} else if (content instanceof byte[]) {
-			return Unpooled.wrappedBuffer((byte[]) content);
-		} else {
-			throw new IllegalArgumentException("unknown content type : "
-					+ content.getClass().getName());
-		}
-	}
-
 	@Override
-	public void send(Channel ch) {
-		ByteBufAllocator alloc = ch.alloc();
-		ByteBuf meta = meta(alloc);
-
-		ByteBuf head = alloc.buffer(HEAD_LEN);
-		head.writeLong(meta.readableBytes() + length);
-		head.writeByte(cmd());
-		head.writeByte(ERRNO_OK);
-
-		CompositeByteBuf cbb = alloc.compositeBuffer();
-		cbb.addComponents(head, meta);
-		cbb.writerIndex(head.readableBytes() + meta.readableBytes());
-		ch.write(cbb);
-
-		ch.writeAndFlush(content);
-	}
-
 	protected ByteBuf meta(ByteBufAllocator alloc) {
 		int metaLen = FDFS_STORE_PATH_INDEX_LEN + FDFS_PROTO_PKG_LEN_SIZE + FDFS_FILE_EXT_LEN;
 		ByteBuf buf = alloc.buffer(metaLen);
 		buf.writeByte(storePathIndex);
-		buf.writeLong(length);
+		buf.writeLong(size);
 		writeFixLength(buf, ext, FDFS_FILE_EXT_LEN);
 		return buf;
 	}
