b75640b4e9e46438ca27e537d0be9e031b68e796
liulongbiao <liulongbiao@cnstrong.cn>
2016-02-26 18:13:41 +0800

    add EmptyDecoder

---
+++ src/main/java/cn/strong/fastdfs/response/EmptyDecoder.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package cn.strong.fastdfs.response;
+
+import io.netty.buffer.ByteBuf;
+
+/**
+ * 空响应解码器
+ * 
+ * @author liulongbiao
+ *
+ */
+public enum EmptyDecoder implements ResponseDecoder<Void> {
+
+	INSTANCE;
+
+	@Override
+	public int expectLength() {
+		return 0;
+	}
+
+	@Override
+	public Void decode(ByteBuf buf) {
+		return null;
+	}
+
+}
